cb465e57344f38976e1417751837c7a6e0ef7562
williampezz <williamaugustopez@gmail.com>
2024-09-26 15:48:51 -0300

    Adicionando o padrao de page-objects

--- src/test/java/br/william/pipeline/firstautomation/pages/HomePage.java
+++ src/test/java/br/william/pipeline/firstautomation/pages/HomePage.java
@@ -3,15 +3,22 @@ package br.william.pipeline.firstautomation.pages;
 import br.william.pipeline.firstautomation.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage {
 
 
-    private  By ButtonSignUp = By.xpath("//a");
+
+    @FindBy (xpath = "//a")
+    WebElement wantBeDogWalker;
 
 
     public HomePage(WebDriver navegador) {
         super(navegador);
+        this.navegador = navegador;
+        PageFactory.initElements(navegador, this);
     }
 
     public void openSite() {
@@ -20,9 +27,9 @@ public class HomePage extends BasePage {
     }
 
     public void clickSignUp() {
-        navegador.findElement(ButtonSignUp).click();
+        wantBeDogWalker.click();
 
     }
 
 
-    }
+}
--- src/test/java/br/william/pipeline/firstautomation/tests/SignUpTests.java
+++ src/test/java/br/william/pipeline/firstautomation/tests/SignUpTests.java
@@ -4,38 +4,39 @@ import br.william.pipeline.firstautomation.core.Hooks;
 import br.william.pipeline.firstautomation.pages.HomePage;
 import br.william.pipeline.firstautomation.pages.SignUpPage;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 @DisplayName("Tests automatizados da funcionalidade de SignUp")
 public class SignUpTests extends Hooks {
 
 
-
-    Hooks hook = new Hooks();
-
-
-
     @Test
     @DisplayName("Registrar um novo usuario com dados validos")
     public void testSignUpUser() throws InterruptedException {
-        hook.setUp();
+
         WebDriver navegador = new ChromeDriver();
 
         HomePage homePage = new HomePage(navegador);
         SignUpPage signUpPage = new SignUpPage(navegador);
 
+
         homePage.openSite();
         homePage.clickSignUp();
-        signUpPage.verifyCadForm();
-        signUpPage.insertDadosForm();
-        signUpPage.insertCEP();
-        signUpPage.fileUpload();
-        signUpPage.clickSignUp();
 
-       // navegador.close();
+        signUpPage.verifyCadForm();
+        signUpPage.enterUsername("William");
+        signUpPage.enterCPF("50954071085");
+        signUpPage.enterEmail("williamteste2@gmail.com");
+        signUpPage.enterCEP("04870470");
+        signUpPage.enterNumber("80");
+        signUpPage.btSearchCep();
+        signUpPage.btActivity();
+
+        signUpPage.fileUpload();
+        signUpPage.btSignUp();
+
+        navegador.close();
 
     }
 
